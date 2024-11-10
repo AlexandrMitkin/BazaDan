0d95524ad2196a6556c3331d6e2f3c21cde7aea3
AlexandrMitkin <ferma1800@yandex.ru>
2024-11-11 00:26:13 +0300

    Add files via upload

--- crud_functions.py
+++ crud_functions.py
@@ -5,17 +5,40 @@ connection = sqlite3.connect('database2.db')
 cursor = connection.cursor()
 
 
+# def initiate_db():
+#     cursor.execute('''
+#         CREATE TABLE IF NOT EXISTS Products(
+#         id INT,
+#         title TEXT,
+#         description TEXT,
+#         price INT
+#         );
+#     ''')
+#     connection.commit()
+
 def initiate_db():
     cursor.execute('''
-        CREATE TABLE IF NOT EXISTS Products(
-        id INT,
-        title TEXT,
-        description TEXT,
-        price INT
+        CREATE TABLE IF NOT EXISTS Users(
+        id INT PRIMARY KEY,
+        username TEXT NOT NULL ,
+        email TEXT NOT NULL,
+        age INT NOT NULL,
+        balance INT DEFAULT 1000
         );
     ''')
     connection.commit()
 
+def add_user(username, email, age):
+    total1=cursor.execute("SELECT COUNT (*) FROM Users").fetchone()[0]+1
+    cursor.execute("INSERT INTO Users (id, username, email, age) VALUES(?, ?, ?, ?)", (total1, username, email, age))
+    connection.commit()
+
+def is_included(username):
+    check_user = cursor.execute('SELECT * FROM Users WHERE id=?', (username,))
+    if check_user.fetchone() is None:
+        return False
+    else:
+        return True
 
 def set_all_products():
     for i in range(4):
