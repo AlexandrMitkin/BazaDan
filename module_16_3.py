9af8d0802ee70faa551b6707ad34c853909daed2
AlexandrMitkin <ferma1800@yandex.ru>
2025-02-01 19:04:33 +0300

    Update module_16_3.py

--- module_16_3.py
+++ module_16_3.py
@@ -1,5 +1,9 @@
+from http.client import HTTPException
+from fastapi import HTTPException
 from fastapi import FastAPI, Path
 from typing import Annotated
+
+from fastapi.openapi.utils import status_code_ranges
 
 app = FastAPI()
 
@@ -17,19 +21,29 @@ async def get_all_messages() -> dict:
 
 
 @app.post("/user/{username}/{age}")
-async def create_messages(username: str, age: int) -> str:
+async def create_messages(username: str= Path(min_length=3, max_length=15, description="Enter your name", example="Vasya"),
+                          age: int=Path(ge=1, le=100, description="Enter your age", example="18")) -> str:
     current_index = str(int(max(users, key=int)) + 1)
     users[current_index] = f"Имя: {username}, возраст: {age}"
     return f"User {current_index} is registered"
 
 
 @app.put("/user/{user_id}/{username}/{age}")
-async def update_messages(user_id: str, username: str, age: int) -> str:
-    users[user_id] = f"Имя: {username}, возраст: {age}"
-    return f"The user {user_id} is updated"
+async def update_messages(user_id: str,
+                          username: Annotated[str,Path(min_length=3, max_length=15, description="Enter your name", example="Vasya")],
+                          age: Annotated[int,Path(ge=1, le=100, description="Enter your age", example="18")]) -> str:
+    for i in users:
+        if i[0]==user_id:
+            users[user_id] = f"Имя: {username}, возраст: {age}"
+            return f"The user {user_id} is updated"
+    raise HTTPException(status_code=404, detail="user not found")
+
 
 
 @app.delete("/user/{user_id}")
 async def delete_massege(user_id: str) -> str:
-    users.pop(user_id)
-    return f"User {user_id} has been deleted"
+    for i in users:
+        if i[0]==user_id:
+            users.pop(user_id)
+            return f"User {user_id} has been deleted"
+    raise HTTPException(status_code=404, detail="user not found")
